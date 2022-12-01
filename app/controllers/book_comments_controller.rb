c32e83832ad65f5e98a83607fed636312a8edb30
EC2 Default User <ec2-user@ip-172-31-4-95.ap-northeast-1.compute.internal>
2022-12-01 07:36:09 +0000

    [finish]応用課題3

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -8,10 +8,15 @@ class BookCommentsController < ApplicationController
     redirect_to book_path(book)
   end
 
+  def destroy
+    BookComment.find(params[:id]).destroy
+    redirect_to book_path(params[:book_id])
+  end
+
   private
 
-  def book_params
-    params.require(:book).permit(:comment)
+  def book_comment_params
+    params.require(:book_comment).permit(:comment)
   end
 
 end
